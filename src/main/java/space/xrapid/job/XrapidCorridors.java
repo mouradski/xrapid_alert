c8bc2dd90484f9f67106338b0de48e1a8130565c
mouradski <mouradski@live.com>
2019-10-21 21:09:26 +0200

    Update delta between XRPL trx and Trades

--- src/main/java/space/xrapid/job/XrapidCorridors.java
+++ src/main/java/space/xrapid/job/XrapidCorridors.java
@@ -90,7 +90,7 @@ public abstract class XrapidCorridors {
         Map<OffsetDateTime, List<XrpTrade>> aggregatedTrades  = xrpTrades.stream()
                 .filter(trade -> getDestinationExchange().equals(exchangeToExchangePayment.getDestination()))
                 .filter(trade -> (trade.getDateTime().toEpochSecond() - exchangeToExchangePayment.getDateTime().toEpochSecond()) > 1)
-                .filter(trade -> (trade.getDateTime().toEpochSecond() - exchangeToExchangePayment.getDateTime().toEpochSecond()) < 60)
+                .filter(trade -> (trade.getDateTime().toEpochSecond() - exchangeToExchangePayment.getDateTime().toEpochSecond()) < 120)
                 .collect(Collectors.groupingBy(XrpTrade::getDateTime));
 
 
