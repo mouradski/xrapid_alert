946c3908dba48c8bfeb0e88d1152f40f791ae1d4
lathan <jucallme@gmail.com>
2019-10-28 14:04:14 -0300

    add some number formatting to the web front end

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -51,7 +51,7 @@ $(function () {
             spottedAt = payment.source;
         }
 
-        msg.innerText = payment.dateTime + ", " + trxClass  + " : " + payment.amount + " XRP from " + payment.source + " to " + payment.destination + ", TrxHash : " + payment.transactionHash +  ", Destination Fiat : " + payment.destinationCurrencry + ", " + spottedAt + " TradeIds : (" + payment.tradeIds + ")";;
+        msg.innerText = payment.dateTime + ", " + trxClass  + " : " + payment.amount.toFixed(6).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " XRP from " + payment.source + " to " + payment.destination + ", TrxHash : " + payment.transactionHash +  ", Destination Fiat : " + payment.destinationCurrencry + ", " + spottedAt + " TradeIds : (" + payment.tradeIds + ")";
 
         scroller.insertBefore(msg, anchor);
     }
