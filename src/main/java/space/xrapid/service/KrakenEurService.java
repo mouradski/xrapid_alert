dd28a6dc484f636fbad7de4a0cd5bc58577a890a
mouradski <mouradski@live.com>
2019-11-15 17:47:47 +0100

    Adding Kraken USD/EUR/CAD support

--- src/main/java/space/xrapid/service/KrakenEurService.java
+++ src/main/java/space/xrapid/service/KrakenEurService.java
@@ -1,11 +1,17 @@
 package space.xrapid.service;
 
 import org.springframework.stereotype.Service;
+import space.xrapid.domain.Exchange;
 
 @Service
 public class KrakenEurService extends KrakenUsdService {
 
     protected String getPair() {
         return "XRPEUR";
+    }
+
+    @Override
+    public Exchange getExchange() {
+        return Exchange.KRAKEN_EUR;
     }
 }
