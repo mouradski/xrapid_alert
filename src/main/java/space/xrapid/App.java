a9ed3bef319e98e14b73d78befd4d808383221bf
mouradski <mouradski@live.com>
2019-12-16 13:11:03 +0100

    Remove unused code

--- src/main/java/space/xrapid/App.java
+++ src/main/java/space/xrapid/App.java
@@ -3,36 +3,9 @@ package space.xrapid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-
-    public static List<String> combinations(List<String> motList, int Longeur) {
-        List<String> Resultat = new ArrayList<String>();
-        for (int i = 0; i < motList.size(); i++) {
-            if (Longeur == 1)
-                Resultat.add(motList.get(i));
-            else {
-
-                try  {
-                    List<String> ListIntermediaire = motList.subList(i + 1, motList.size() - (i + 1));
-                    List<String> CombiList = combinations(ListIntermediaire, Longeur - 1);
-                    for (String s : CombiList) {
-                        Resultat.add(motList.get(i) + s);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-        return Resultat;
     }
 }
