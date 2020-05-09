008724d30bd6dbb76f87953a8b7f023bc7f93080
mouradski <mouradski@live.com>
2020-05-09 20:21:18 +0200

    Adding flags

--- src/main/webapp/src/app/components/map/map.component.ts
+++ src/main/webapp/src/app/components/map/map.component.ts
@@ -6,6 +6,7 @@ import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 import {TablesService} from "../../pages/tables/tables.service";
 import { Queue } from 'queue-typescript';
 import {Payment} from "../../pages/tables/tables.component";
+import {DeviceDetectorService} from "ngx-device-detector";
 
 
 @Component({
@@ -17,7 +18,9 @@ export class MapComponent implements AfterViewInit {
     mapChart: any;
     am4maps: any;
     cities: any;
+    cityImages: any;
     lineSeries: any;
+    public mobile: boolean;
 
     queue:Queue<any> = new Queue<any>();
 
@@ -53,8 +56,6 @@ export class MapComponent implements AfterViewInit {
             console.log("old corridor");
         }
 
-        // console.log(this.addLine(this.currencies.get(source), this.currencies.get(destination)));
-
 
         this.showOdl(this.corridors.get(corridor));
     }
@@ -79,14 +80,11 @@ export class MapComponent implements AfterViewInit {
             polygonSeries.mapPolygons.template.nonScalingStroke = true;
             polygonSeries.exclude = ["AQ"];
 
+            this.cityImages = this.mapChart.series.push(new am4maps.MapImageSeries());
+            this.cityImages.mapImages.template.nonScaling = false;
+            this.cityImages.zIndex = 1;
 
-
-
-            this.cities = this.mapChart.series.push(new am4maps.MapImageSeries());
-            this.cities.mapImages.template.nonScaling = true;
-            this.cities.zIndex = 1;
-
-            let citiesTemplate = this.cities.mapImages.template;
+            let citiesTemplate = this.cityImages.mapImages.template;
             let city = citiesTemplate.createChild(am4core.Image);
             city.width = 28;
             city.height = 28;
@@ -95,13 +93,14 @@ export class MapComponent implements AfterViewInit {
             city.tooltipText = "{title}";
             city.horizontalCenter = "middle";
             city.verticalCenter = "middle";
+            city.zIndex = 10;
             city.propertyFields.href = "flag";
             // Set property fields
             citiesTemplate.propertyFields.latitude = "latitude";
             citiesTemplate.propertyFields.longitude = "longitude";
 
             // Add data for the cities
-            this.cities.data = [{
+            this.cityImages.data = [{
                 "latitude": -35.2820,
                 "longitude": 149.1286,
                 "title": "Australia",
@@ -136,8 +135,18 @@ export class MapComponent implements AfterViewInit {
                 "longitude": -43.2096,
                 "title": "Brasil",
                 "flag": "https://cdn.countryflags.com/thumbs/brazil/flag-3d-round-500.png"
-            }
+            },
+                {
+                    "latitude": 50.5101,
+                    "longitude": 4.2055,
+                    "title": "Europe",
+                    "flag": "https://cdn.quincaillerie.pro/images/4f4b0799128b63bf9243/0/0/P211854.png"
+                }
             ];
+
+
+            this.cities = this.mapChart.series.push(new am4maps.MapImageSeries());
+            this.cities.mapImages.template.nonScaling = true;
 
 
             this.currencies.set("USD", this.addCity({ "latitude": 42.7392, "longitude":-85.9902 }, "United-States"));
@@ -147,6 +156,7 @@ export class MapComponent implements AfterViewInit {
             this.currencies.set("THB", this.addCity({"latitude": 13.7367,"longitude": 100.5231},  "Thailand"));
             this.currencies.set("KRW", this.addCity({"latitude": 37.5326,"longitude": 127.0246},  "Korea"));
             this.currencies.set("BRL", this.addCity({"latitude": -22.9035,"longitude": -43.2096},  "Brasil"));
+            this.currencies.set("EUR", this.addCity({"latitude": 50.5101,"longitude": 4.2055},  "Europe"));
 
             this.lineSeries = this.mapChart.series.push(new am4maps.MapArcSeries());
             this.lineSeries.mapLines.template.line.strokeWidth = 3;
@@ -188,33 +198,31 @@ export class MapComponent implements AfterViewInit {
         bullet.width = 48;
         bullet.height = 48;
 
+
         let plane = bullet.createChild(am4core.Sprite);
         plane.scale = 0.01;
         plane.horizontalCenter = "middle";
         plane.verticalCenter = "middle";
-        //plane.path = "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47";
-        plane.path = "M1297.93 53.81c-131.41 77.2-209.22 216.61-209.22 363.62 0 77.2 31 155 69.81 224.41 31 62 46.2 170.21-62 224.41-77.21 46.2-178.22 15.4-224.42-62-46.21-62-100.41-124-170.22-170.21-131.41-77.2-286.43-77.2-417.85 0S75 851 75 998.05s77.21 286.41 209 363.82c131.41 77.2 286.43 77.2 417.85 0 69.81-38.8 124-100.4 162.42-170.21 31-54.2 116.21-124 224.42-62 77.21 46.2 100.41 147.21 62 224.41-38.8 69.8-62 147.21-62 224.41 0 147.21 77.21 286.41 209.22 363.62 131.41 77.2 286.43 77.2 417.85 0S1925 1725.49 1925 1578.48s-77.41-286.41-209.22-363.82c-69.81-38.8-147.22-54.2-232.23-54.2-69.81 0-162.42-46.2-162.42-162.41 0-93 69.81-162.41 162.42-162.41 77.21 0 162.42-15.4 232.23-54.2C1847.19 704.24 1925 564.83 1925 417.83s-77.41-286.41-209.22-363.62c-62-38.8-139.22-54.2-209-54.2-69.41-.4-147.22 15.4-208.82 53.8";
-        plane.fill = am4core.color("#d70c0c");
+        plane.path = "M983 2240 c-265 -37 -485 -150 -669 -342 -262 -274 -366 -645 -284 -1019 44 -202 135 -371 285 -528 159 -168 350 -274 588 -328 99 -22 345 -22 444 0 238 54 429 160 588 328 91 95 135 157 191 266 83 163 118 313 118 508 0 195 -35 345 -118 508 -156 306 -439 521 -779 593 -88 18 -280 26 -364 14z m-196 -666 c203 -212 238 -236 339 -236 97 0 135 26 339 238 l177 184 99 0 c54 0 99 -3 99 -7 0 -8 -102 -115 -324 -341 -204 -207 -239 -227 -391 -227 -153 0 -188 20 -396 232 -225 229 -319 328 -319 336 0 4 45 7 99 7 l99 0 179 -186z m463 -520 c79 -25 130 -69 369 -316 l233 -243 -103 -3 -103 -3 -188 195 c-169 175 -194 197 -246 216 -42 16 -70 20 -104 16 -94 -10 -123 -32 -318 -234 l-184 -192 -104 0 -104 0 71 78 c39 42 151 159 248 258 142 144 190 187 235 209 93 44 197 51 298 19z";
+        plane.fill = am4core.color("#000000");
         plane.strokeOpacity = 0;
+        plane.zIndex = 60;
 
         this.goPlane(bullet, plane);
     }
 
-    constructor(private httpClient: HttpClient, private zone: NgZone, private tablesService: TablesService) {
+    constructor(private httpClient: HttpClient, private zone: NgZone, private tablesService: TablesService, private deviceService: DeviceDetectorService) {
+        this.mobile = this.deviceService.isMobile();
         const _this = this;
         this.tablesService.getSingleData().subscribe(data => {
 
-            //_this.notifyOdl(data.sourceFiat, data.destinationFiat);
             _this.queue.enqueue(data);
 
         });
 
         this.tablesService.getData().subscribe(data => {
 
-            for (let i = 0; i < data.length && i < 5; i++) {
-                console.log(i);
-                _this.queue.enqueue(data[i]);
-            }
+            _this.queue.enqueue(data[0]);
 
         });
 
